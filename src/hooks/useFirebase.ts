3794d799a38fefef02ea3718571f23c0ae554d2f
Guapura89 <betocrack1228@gmail.com>
2024-02-04 20:57:33 -0700

    feat: add functions to get daily and yesterday champs

--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -14,6 +14,7 @@ import {
 } from 'firebase/firestore';
 import { db } from '@/firebaseConfig';
 import { Champ } from './useFirebase.model';
+import { generateDailyNumber, generateYesterdayNumber } from '@/utils/getRandomNumber';
 
 export const useFirebase = () => {
   const getAllChamps = async () => {
@@ -36,6 +37,18 @@ export const useFirebase = () => {
     return data[0];
   };
 
+  const getDailyChamp = async () => {
+    const champs = await getAllChamps();
+    const daily = champs.filter((item, index) => index === generateDailyNumber());
+    return daily;
+  };
+
+  const getYesterdayChamp = async () => {
+    const champs = await getAllChamps();
+    const yesterday = champs.filter((item, index) => index === generateYesterdayNumber());
+    return yesterday;
+  };
+
   const addChamp = (data: Champ) => {
     addDoc(collection(db, 'champs'), data)
       .then((res) => {
@@ -51,6 +64,8 @@ export const useFirebase = () => {
   return {
     getAllChamps,
     getChampByName,
+    getDailyChamp,
+    getYesterdayChamp,
     addChamp,
   };
 };
